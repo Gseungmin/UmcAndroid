739d6bccce6be18d2ff8dfab42d1ef89e0b95d48
Ji Seungmin <cswcsm02@gmail.com>
2022-10-30 23:49:06 +0900

    액티비티에서 리포지토리 사용 선언

--- app/src/main/java/com/example/umc/ui/MainActivity.kt
+++ app/src/main/java/com/example/umc/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.umc.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.umc.databinding.ActivityMainBinding
+import com.example.umc.db.MemoDatabase
+import com.example.umc.repository.MemoRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val database = MemoDatabase.getInstance(this)
+        //datastore 의존성을 repository에 전달
+        val memoRepository = MemoRepositoryImpl(database)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
