37a264fefb31655b4823ae8b694576299e4baed0
Ji Seungmin <cswcsm02@gmail.com>
2022-10-31 11:40:49 +0900

    RecyclerView에 Room과 LiveData 적용 후 컴파일 에러로 인한 잠시 바활성화

--- app/src/main/java/com/example/umc/ui/MainFragment.kt
+++ app/src/main/java/com/example/umc/ui/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,12 +13,14 @@ import com.example.challengesecond.adapter.MemoModel
 import com.example.challengesecond.adapter.MemoRVAdapter
 import com.example.umc.R
 import com.example.umc.databinding.FragmentMainBinding
+import com.example.umc.viewmodel.MemoViewModel
 
 class MainFragment : Fragment() {
 
     private var _binding : FragmentMainBinding? = null
     private val binding get() = _binding!!
     private lateinit var memoRVAdapter: MemoRVAdapter;
+    private lateinit var memoViewModel: MemoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,34 +33,17 @@ class MainFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        //RvItem 초기화
-        val items = mutableListOf<MemoModel>()
-        items.add(MemoModel("pic4"))
-        items.add(MemoModel("pic5"))
-        items.add(MemoModel("pic6"))
-        items.add(MemoModel("pic7"))
-        items.add(MemoModel("pic8"))
-        items.add(MemoModel("pic9"))
-        items.add(MemoModel("pic10"))
-        items.add(MemoModel("pic11"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic12"))
-        items.add(MemoModel("pic13"))
+        memoViewModel = (activity as MainActivity).viewModel
+
+        memoViewModel.getAllMemo()
 
         //RecyclerView 기능
-        memoRVAdapter = MemoRVAdapter(items)
-        val memo_rv : RecyclerView = binding.rv
-        memo_rv.adapter = memoRVAdapter
-        memo_rv.layoutManager = LinearLayoutManager(requireContext())
+//        memoViewModel.memoList.observe(viewLifecycleOwner) {
+//            memoRVAdapter = MemoRVAdapter(it)
+//            val memo_rv : RecyclerView = binding.rv
+//            memo_rv.adapter = memoRVAdapter
+//            memo_rv.layoutManager = LinearLayoutManager(requireContext())
+//        }
 
         //네비게이션 기능
         binding.saveBtn.setOnClickListener {
