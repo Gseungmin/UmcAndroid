0547cde93ace6831ad9f37f84ddca7c9008fa1b7
Ji Seungmin <cswcsm02@gmail.com>
2022-11-06 02:39:14 +0900

    FEAT: CardStackView Depengency 추가

--- app/src/main/java/com/example/umc/ui/HomeFragment.kt
+++ app/src/main/java/com/example/umc/ui/HomeFragment.kt
@@ -22,7 +22,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater)
         val view = binding.root
