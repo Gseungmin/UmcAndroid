ee31ae71589b5f4acf23921c98707d551f089806
Ji Seungmin <cswcsm02@gmail.com>
2022-11-09 23:16:49 +0900

    REFACTOR: Activity와 Fragment ViewBinding 초기화

---
+++ app/src/main/java/com/example/umc/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.example.umc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.umc.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding : FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
