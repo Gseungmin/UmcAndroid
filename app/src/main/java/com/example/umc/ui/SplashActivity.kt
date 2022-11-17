9e1e7c33a298dbe901719dfb5e4a28ae1d28e4ce
Ji Seungmin <cswcsm02@gmail.com>
2022-11-17 20:13:19 +0900

    REFACTOR: ProgressBar 기능 구현 코드 Refactor

--- app/src/main/java/com/example/umc/ui/SplashActivity.kt
+++ app/src/main/java/com/example/umc/ui/SplashActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.umc.databinding.ActivitySplashBinding
@@ -22,17 +24,11 @@ class SplashActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        viewModel = ViewModelProvider(this).get(LoadingViewModel::class.java)
-
-        viewModel.countValue.observe(this, Observer {
-            binding.num.text = it.toString()
-            if (it.equals("로딩 중...")) {
-                Handler().postDelayed({
-                    startActivity(Intent(this, MainActivity::class.java))
-                    finish()
-                },10)
-            }
-        })
-        viewModel.exec()
+        Handler(Looper.myLooper()!!).postDelayed({
+            binding.progressBar.isVisible = false
+            binding.textViewIsLoading.isVisible = false
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
