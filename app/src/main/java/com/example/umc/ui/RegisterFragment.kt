6cc25db73fd35c6c7fd209f3a642dbd99386a764
Ji Seungmin <cswcsm02@gmail.com>
2022-11-17 22:39:03 +0900

    FEAT: Register View 생성 및 Navigation 기능 구현

--- app/src/main/java/com/example/umc/ui/RegisterFragment.kt
+++ app/src/main/java/com/example/umc/ui/RegisterFragment.kt
@@ -5,16 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.viewpager2.widget.ViewPager2
 import com.example.umc.R
-import com.example.umc.databinding.FragmentHomeBinding
-import com.example.umc.databinding.FragmentOrderBinding
-import com.example.umc.slider.PagerAdapter
+import com.example.umc.databinding.FragmentRegisterBinding
 
-class OrderFragment : Fragment() {
 
-    private lateinit var binding: FragmentOrderBinding
+class RegisterFragment : Fragment() {
+
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +25,14 @@ class OrderFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
 
-        binding = FragmentOrderBinding.inflate(layoutInflater)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         val view = binding.root
 
-        binding.register.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_fragment_order_to_registerFragment)
-        }
-
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
