ab8234b6f0e8c2427f04d38134a345621a2e7f79
Ji Seungmin <cswcsm02@gmail.com>
2022-11-26 20:47:47 +0900

    위치 값을 받아 지도를 이동시키는 메소드 추가

--- app/src/main/java/com/example/umc/ui/UserFragment.kt
+++ app/src/main/java/com/example/umc/ui/UserFragment.kt
@@ -1,17 +1,44 @@
 package com.example.umc.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.umc.R
 import com.example.umc.databinding.FragmentOrderBinding
 import com.example.umc.databinding.FragmentUserBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
 
 class UserFragment : Fragment() {
 
     private lateinit var binding: FragmentUserBinding
+
+    //허용받은 권한 목록
+    val permission_list = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+
+    //gps 정보를 관리할 매니저
+    private lateinit var manager : LocationManager
+
+    //위치정보 측정 성공시 호출될 메소드가 구현되어 있는 리스너
+    //위치측정을 한번 하게되면 다시 측정하지 않게 연결을 끊을 것이므로 전역 변수로 관리
+    private lateinit var locationListener: LocationListener
+
+    //구글맵을 제어해야 하므로 구글맵 객체도 받아옴
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +55,19 @@ class UserFragment : Fragment() {
         binding = FragmentUserBinding.inflate(layoutInflater)
         val view = binding.root
         return view
+    }
+
+    // 위치 값을 받아 지도를 이동시키는 메소드
+    private fun setUserLocation(location: Location) {
+
+        //위치 측정을 중단
+        manager.removeUpdates(locationListener)
+
+        //위도와 경도 값을 관리하는 객체
+        val loc1 = LatLng(location.latitude, location.longitude)
+        //지도를 이동시키기 위한 객체 생성
+        val loc2 = CameraUpdateFactory.newLatLngZoom(loc1, 15f)
+        // 이동
+        googleMap.animateCamera(loc2)
     }
 }
