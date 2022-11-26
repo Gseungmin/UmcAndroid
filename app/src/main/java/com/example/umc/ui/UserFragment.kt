1e3095ecc31bb0319cc1e078452d6dd34756c7e4
Ji Seungmin <cswcsm02@gmail.com>
2022-11-26 20:51:53 +0900

    현재 위치를 측정하는 메소드 추가

--- app/src/main/java/com/example/umc/ui/UserFragment.kt
+++ app/src/main/java/com/example/umc/ui/UserFragment.kt
@@ -1,6 +1,7 @@
 package com.example.umc.ui
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -69,5 +70,48 @@ class UserFragment : Fragment() {
         val loc2 = CameraUpdateFactory.newLatLngZoom(loc1, 15f)
         // 이동
         googleMap.animateCamera(loc2)
+    }
+
+    //현재 위치를 측정하는 메소드
+    private fun getMyLocation() {
+        // 위치 정보를 관리하는 매니저 추출
+        manager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // 저장되어 있는 위치값이 있으면 가져옴
+        // 먼저 권한 체크부터 실행
+        val auth1 = ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+        val auth2 = ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        //허용을 하지 않은 경우 함수 종료
+        if ((auth1 != PackageManager.PERMISSION_GRANTED) && (auth2 != PackageManager.PERMISSION_GRANTED)) {
+            return
+        }
+
+        //권한이 허용된 경우
+        //GPS를 이용해 데이터 확인
+        val location1 = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        //네트워크 망을 통해 데이터 확인
+        val location2 = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+        //새로운 위치 측정 요청
+        locationListener = LocationListener {
+            setUserLocation(it)
+        }
+
+        //이미 저장된 위치 셋팅
+        if (location1 != null) {
+            setUserLocation(location1)
+        } else if (location2 != null) {
+            setUserLocation(location2)
+        }
+
+        //GPS를 이용할 수 있는 환경이라면, NETWORK를 이용할 수 있는 환경이라면
+        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER) == true) {
+            manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        } else if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) == true) {
+            manager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+        }
     }
 }
