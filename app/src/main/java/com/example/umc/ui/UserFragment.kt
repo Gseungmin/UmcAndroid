feec522aefccb12b4aec9c7fd71626de4cae68da
Ji Seungmin <cswcsm02@gmail.com>
2022-11-26 23:34:28 +0900

    dialog 기능 추가

--- app/src/main/java/com/example/umc/ui/UserFragment.kt
+++ app/src/main/java/com/example/umc/ui/UserFragment.kt
@@ -1,6 +1,7 @@
 package com.example.umc.ui
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
@@ -23,6 +24,32 @@ import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 
 class UserFragment : Fragment(), OnMapReadyCallback {
+
+    //장소 유형 리스트
+    val dialogData = arrayOf(
+        "accounting", "airport", "amusement_park",
+        "aquarium", "art_gallery", "atm", "bakery",
+        "bank", "bar", "beauty_salon", "bicycle_store",
+        "book_store", "bowling_alley", "bus_station",
+        "cafe", "campground", "car_dealer", "car_rental",
+        "car_repair", "car_wash", "casino", "cemetery",
+        "church", "city_hall", "clothing_store", "convenience_store",
+        "courthouse", "dentist", "department_store", "doctor",
+        "drugstore", "electrician", "electronics_store", "embassy",
+        "fire_station", "florist", "funeral_home", "furniture_store",
+        "gas_station", "gym", "hair_care", "hardware_store", "hindu_temple",
+        "home_goods_store", "hospital", "insurance_agency",
+        "jewelry_store", "laundry", "lawyer", "library", "light_rail_station",
+        "liquor_store", "local_government_office", "locksmith", "lodging",
+        "meal_delivery", "meal_takeaway", "mosque", "movie_rental", "movie_theater",
+        "moving_company", "museum", "night_club", "painter", "park", "parking",
+        "pet_store", "pharmacy", "physiotherapist", "plumber", "police", "post_office",
+        "primary_school", "real_estate_agency", "restaurant", "roofing_contractor",
+        "rv_park", "school", "secondary_school", "shoe_store", "shopping_mall",
+        "spa", "stadium", "storage", "store", "subway_station", "supermarket",
+        "synagogue", "taxi_stand", "tourist_attraction", "train_station",
+        "transit_station", "travel_agency", "university", "eterinary_care","zoo"
+    )
 
     //허용받은 권한 목록
     val permission_list = arrayOf(
@@ -67,6 +94,44 @@ class UserFragment : Fragment(), OnMapReadyCallback {
         Log.d("MAP1", mapView.toString())
 
         mapView.getMapAsync(this)
+
+        binding.dialog.setOnClickListener {
+            val placeListBuilder = AlertDialog.Builder(requireContext())
+            placeListBuilder.setTitle("장소 종류 선택")
+            placeListBuilder.setNegativeButton("취소", null)
+            placeListBuilder.setNeutralButton("초기화") { dialogInterface, i ->
+//                //마커 제거
+//                for (m in nearby_marker) {
+//                    m.remove()
+//                }
+//
+//                //리스트 초기화
+//                nearby_lat.clear()
+//                nearby_name.clear()
+//                nearby_vicinity.clear()
+//                nearby_log.clear()
+//                nearby_marker.clear()
+            }
+            placeListBuilder.setItems(dialogData) {
+                    dialogInterface, i ->
+//
+//                //마커 제거
+//                for (m in nearby_marker) {
+//                    m.remove()
+//                }
+//
+//                //리스트 초기화
+//                nearby_lat.clear()
+//                nearby_name.clear()
+//                nearby_vicinity.clear()
+//                nearby_log.clear()
+//                nearby_marker.clear()
+//
+//                getNearbyPlaceData(dialogData[i])
+            }
+
+            placeListBuilder.show()
+        }
 
         return view
     }
