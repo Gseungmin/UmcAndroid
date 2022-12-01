43918b63098069fdd89a40048391349841f160f4
Ji Seungmin <cswcsm02@gmail.com>
2022-12-02 00:57:25 +0900

    ViewModel 설정

--- app/src/main/java/com/example/umc/repository/DataRepository.kt
+++ app/src/main/java/com/example/umc/repository/DataRepository.kt
@@ -1,7 +1,6 @@
 package com.example.umc.repository
 
 import androidx.datastore.core.DataStore
-import androidx.datastore.dataStore
 import androidx.datastore.preferences.core.*
 import com.example.umc.Sort
 import com.example.umc.db.DataBase
@@ -9,7 +8,6 @@ import com.example.umc.db.DataEntity
 import com.example.umc.repository.DataRepository.PreferencesKeys.SORT_MODE
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.internal.NopCollector.emit
 import kotlinx.coroutines.flow.map
 import java.io.IOException
 
@@ -29,7 +27,6 @@ class DataRepository(private val db: DataBase,
     // 단순히 string을 사용하던 sharedPreference와 다르게 타입 안정을 위해 및 저장할 데이터가 String이기 때문에 stringPreferencesKey 사용
     private object PreferencesKeys {
         val SORT_MODE = stringPreferencesKey("sort_mode")
-        val CACHE_DELETE_MODE = booleanPreferencesKey("cache_delete_mode")
     }
 
     //저장 작업은 코루틴 안에서 이루어져야 하므로 suspend 사용 및 전달받을 Mode 값을 edit 블록 안에서 저장
--- app/src/main/java/com/example/umc/viewmodel/ImageViewModel.kt
+++ app/src/main/java/com/example/umc/viewmodel/ImageViewModel.kt
@@ -2,6 +2,7 @@ package com.example.umc.viewmodel
 
 import android.graphics.Bitmap
 import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,7 +10,9 @@ import androidx.lifecycle.viewModelScope
 import com.example.umc.db.DataEntity
 import com.example.umc.repository.DataRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ImageViewModel(private val repository: DataRepository) : ViewModel() {
 
@@ -19,6 +22,7 @@ class ImageViewModel(private val repository: DataRepository) : ViewModel() {
 
     fun getData() = viewModelScope.launch(Dispatchers.IO) {
         _imageList.postValue(repository.getAllData())
+        getSortMode()
     }
 
     fun insertData(bitmap: Bitmap, title: String, location: String, date: String) =
@@ -38,5 +42,20 @@ class ImageViewModel(private val repository: DataRepository) : ViewModel() {
 
     fun setImage(images: List<Uri>) {
         _datas.value = images
+    }
+
+    //DataStore
+    //값을 저장
+    //repository의 saveSortMode를 viewModelScope에서 실행하되 IO 작업이므로 Dispatchers를 IO로 설정
+    fun saveSortMode(value: String) = viewModelScope.launch(Dispatchers.IO) {
+        repository.saveSortMode(value)
+    }
+
+    //값을 불러옴
+    suspend fun getSortMode() = withContext(Dispatchers.IO) {
+        //설정 값 특성상 전체 데이터 스트림을 가져올 필요 없음
+        //withContext는 반드시 값을 반환하고 종료됨
+        val data = repository.getSortMode().first()
+        Log.d("Value", data)
     }
 }
